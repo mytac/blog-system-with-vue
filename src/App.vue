57b0328c76468f1fbf92f0bd67ee236f38a863ae
Tactics Chang <my_tac@126.com>
2017-01-15 21:55:55 +0800

    add animate structure

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+var VueRouter = require('vue-router')
+
+Vue.use(VueRouter)
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App }
+})
