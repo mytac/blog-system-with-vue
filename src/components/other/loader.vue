59d74981894dc99b976160562753be3743769eba
Tactics Chang <my_tac@126.com>
2017-01-18 20:30:44 +0800

    rebind

---
+++ src/components/other/loader.vue
@@ -0,0 +1,18 @@
+<style scoped>
+
+</style>
+<template>
+    <div>
+        <div id="loader-2">
+            <span></span><span></span><span></span><span></span><span></span>
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default{
+        data () {
+            return {}
+        }
+    }
+</script>
