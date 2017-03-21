f75ab3483a231abb3d98f9925fac7431bc2cb86b
mytac <my_tac@126.com>
2017-03-21 17:08:23 +0800

    【editor】autosave fn

--- src/components/edit/edit.vue
+++ src/components/edit/edit.vue
@@ -1,7 +1,13 @@
 <style></style>
 <template>
   <div>
-    <markdown-editor :value.sync="content" v-ref:markdown-editor></markdown-editor>
+    <i-form :model="formItem" :label-width="80">
+      <Form-item label="文章标题">
+        <i-input :value.sync="formItem.input" placeholder="请输入"></i-input>
+      </Form-item>
+      <markdown-editor :value.sync="content" v-ref:markdown-editor></markdown-editor>
+    </i-form>
+
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -18,14 +24,22 @@
             configs: {
               spellChecker: false // 禁用拼写检查
             },
-            timeOut:0
+            timeOut:0,
+            formItem:{
+              input:''
+            },
+            temp:''
           }
         )
     },
     watch:{
         'content':function(val,oldVal){
-            console.log(val)
-          console.log(oldVal)
+          this.temp=oldVal
+          setTimeout(() => {
+              if(val==this.temp){
+                  this.autoSave(val)
+              }
+          },5000)
         }
     },
     methods:{
