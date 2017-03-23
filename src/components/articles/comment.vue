9d9432d62ea0e0cc6c4c4e854892bdd0911d4aed
mytac <my_tac@126.com>
2017-03-23 16:52:45 +0800

    【comment.vue】 init

---
+++ src/components/articles/comment.vue
@@ -0,0 +1,42 @@
+<style></style>
+<template>
+  <div v-if="!showComment">
+    <h1>暂无评论</h1>
+  </div>
+  <div class="comment" v-if="showComment">
+    <div v-for="c in comments">
+      <div>
+        <div class="author"><a  target="_blank" class="avatar"><img></a>
+          <div class="info"><a  target="_blank" class="name">{{c.userName}}</a> <!---->
+            <div class="meta"><span>{{c.createDate}}</span></div>
+          </div>
+        </div>
+        <div class="comment-wrap"><p>{{c.comment}}</p>
+          <div class="tool-group"><a><i class="iconfont ic-zan"></i></a> <a><i
+            class="iconfont ic-comment"></i><span>回复</span></a> <a class="report"></a> <!----></div>
+        </div>
+      </div>
+      <div class="sub-comment-list hide"><!----> <!----></div>
+    </div>
+
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default{
+      data(){
+          return ({
+            showComment:false
+          })
+      },
+    props:{
+          comments:{
+              type:Array
+          }
+    },
+    ready(){
+        if(this.comments[0]){
+            this.showComment=true
+        }
+    }
+  }
+</script>
--- src/components/articles/detail.vue
+++ src/components/articles/detail.vue
@@ -1,20 +1,30 @@
 <style scoped>
-  #content{
-    height:500px;
+  #content {
+    /*height: 500px;*/
   }
-  .topBar{margin-top:20px;}
-  .demo-affix{
+
+  .topBar {
+    margin-top: 20px;
+  }
+
+  .demo-affix {
     display: inline-block;
-    width:80px;
-    height:50px;
+    width: 80px;
+    height: 50px;
     /*background-color: #000000;*/
-    font-size:30px;
-    line-height:30px;
+    font-size: 30px;
+    line-height: 30px;
     vertical-align: middle;
     text-align: center;
-    border:1px solid #e6e6e6;
+    border: 1px solid #e6e6e6;
     border-radius: 15px;
     cursor: pointer;
+  }
+  .switch{
+    margin:50px auto;
+  }
+  .comments{
+    margin-top:35px;
   }
 </style>
 <template>
@@ -27,43 +37,68 @@
     </Affix>
     <div class="topBar">
       <h1>{{article.title}}</h1>
-      <h3>{{article.createDate}}   <span>{{article.userId}}</span></h3>
+      <h3>{{article.createDate}}<span>{{article.userId}}</span></h3>
     </div>
     <div id="content"></div>
+  </div>
+  <div class="switch">
+    <Switch @on-change="openComment"></Switch>展开评论
+  </div>
+  <div class="comments" v-if="showComment">
+    <comment :comments="comments"></comment>
   </div>
 
 </template>
 <script type="text/ecmascript-6">
-
+import comment from './comment.vue'
   const markdown = require('markdown').markdown
   const $ = require('jquery')
   export default{
-      data(){
-          return({
-              article:{}
-          })
-      },
-    methods:{
+    data(){
+      return ({
+        article: {},
+        showComment:false,
+        comments:[]
+      })
+    },
+    methods: {
       likeIt(){
-          let id=this.article.likeNum
+        let id = this.article.likeNum
         //ajax here...
+        //article/likeIt
 
+      },
+      openComment(){
+        //ajax here...
+        //article/comments
+        let comments=[
+          {userid:'1',userName:'小花',comment:'大赛的静安寺的骄傲的三大',createDate:'2013-01-20'},
+          {userid:'1',userName:'小花',comment:'大赛的静安寺的骄傲的三大',createDate:'2013-01-20'},
+          {userid:'1',userName:'小花',comment:'大赛的静安寺的骄傲的三大',createDate:'2013-01-20'},
+          {userid:'1',userName:'小花',comment:'大赛的静安寺的骄傲的三大',createDate:'2013-01-20'},
+          {userid:'1',userName:'小花',comment:'大赛的静安寺的骄傲的三大',createDate:'2013-01-20'}
+        ]
+        this.comments=comments
+        this.showComment=!this.showComment
       }
     },
-      ready(){
-          console.log(this.$route.params.articleId)
-        //ajax here...
-        //article/showDetail
-        let article={ //temp
-              content:'Hello.\n\n* This is markdown.\n* It is fun\n* Love it or leave it.',
-              title:'按时发放水电费',
-              userId:'康华大厦',
-              likeNum:20,
-              createDate:'2013-56-12'
-            }
-        this.article=article
-        let html_content = markdown.toHTML( article.content);
-          $('#content').html(html_content)
+    components:{
+        'comment':comment
+    },
+    ready(){
+      console.log(this.$route.params.articleId)
+      //ajax here...
+      //article/showDetail
+      let article = { //temp
+        content: 'Hello.\n\n* This is markdown.\n* It is fun\n* Love it or leave it.',
+        title: '按时发放水电费',
+        userId: '康华大厦',
+        likeNum: 20,
+        createDate: '2013-56-12'
       }
+      this.article = article
+      let html_content = markdown.toHTML(article.content);
+      $('#content').html(html_content)
+    }
   }
 </script>
